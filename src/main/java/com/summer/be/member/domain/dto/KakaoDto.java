8a3e2a55edb99d8faa726af056e830f16133573b
BuKwon Kim <dnu05043@naver.com>
2024-06-26 23:35:12 +0900

    feat: 카카오 로그인

---
+++ src/main/java/com/summer/be/member/domain/dto/KakaoDto.java
@@ -0,0 +1,18 @@
+package com.summer.be.member.domain.dto;
+
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class KakaoDto {
+    private Long id;
+    private String nickname;
+    private String profileImg;
+    private String email;
+}
