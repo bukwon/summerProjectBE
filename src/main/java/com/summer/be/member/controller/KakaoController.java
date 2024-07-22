701554e9dc869792737a0d9f9804dd763b62503f
t05h19@naver.com <t05h19@naver.com>
2024-07-22 21:18:40 +0900

    feat: swagger 설정

--- src/main/java/com/summer/be/member/controller/KakaoController.java
+++ src/main/java/com/summer/be/member/controller/KakaoController.java
@@ -2,20 +2,34 @@ package com.summer.be.member.controller;
 
 import com.summer.be.member.domain.dto.KakaoDto;
 import com.summer.be.member.service.KakaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Tag(name = "User", description = "User 관련 API 입니다.")
 @RequiredArgsConstructor
-@Controller
+@RestController
+@CrossOrigin
+@RequestMapping("/api/user")
 public class KakaoController {
     private final KakaoService kakaoService;
+
+    @Operation(
+            summary = "회원가입",
+            description = "회원가입을 합니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "회원가입에 성공하였습니다."
+    )
+
     @RequestMapping("/login/oauth2/code/kakao")
     public String kakaoLogin(@RequestParam("code") String code) throws IOException {
         // 1. 인가 코드 받기 (@RequestParam String code)
