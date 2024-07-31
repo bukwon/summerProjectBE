f85a12330353c195365529efa966e3b18692e890
t05h19@naver.com <t05h19@naver.com>
2024-07-31 21:10:55 +0900

    fix: MemberController 스웨거에서 볼 수 있도록

--- src/main/java/com/summer/be/member/controller/MemberController.java
+++ src/main/java/com/summer/be/member/controller/MemberController.java
@@ -1,37 +1,37 @@
 package com.summer.be.member.controller;
 
+import com.summer.be.member.domain.Member;
 import com.summer.be.member.domain.dto.MemberDto;
+import com.summer.be.member.domain.dto.RequestLoginDto;
 import com.summer.be.member.service.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Tag(name = "User", description = "User 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin
+@RequestMapping("/api/user")
 public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody MemberDto memberDto) {
-        try{
-            memberService.login(memberDto.getEmail(), memberDto.getPassword());
-            return ResponseEntity.ok().build();
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<?> login(@RequestBody RequestLoginDto requestLoginDto) {
+        Member member = memberService.login(requestLoginDto.getEmail(), requestLoginDto.getPassword());
+
+        return ResponseEntity.ok(member.getNickname());
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody MemberDto memberDto) {
         try {
-            memberService.signUp(memberDto.getEmail(), memberDto.getPassword());
+            memberService.signUp(memberDto.getEmail(), memberDto.getNickname(), memberDto.getPassword());
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
