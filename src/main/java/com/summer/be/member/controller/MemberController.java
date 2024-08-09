920b5eb329b5f9f05f50b6a49b29cfaf18819216
t05h19@naver.com <t05h19@naver.com>
2024-08-09 15:29:34 +0900

    fix: 회원가입과 로그인 일원화

--- src/main/java/com/summer/be/member/controller/MemberController.java
+++ src/main/java/com/summer/be/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.summer.be.member.controller;
 
 import com.summer.be.member.domain.Member;
 import com.summer.be.member.domain.dto.KakaoAccountIdDto;
+import com.summer.be.member.domain.dto.LoginResponseDto;
 import com.summer.be.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -25,7 +26,7 @@ public class MemberController {
 
     @Operation(
             summary = "로그인",
-            description = "카카오 계정 아이디로 로그인을 합니다."
+            description = "카카오 계정 아이디로 로그인을 합니다. 계정 정보가 없을 경우 생성합니다."
     )
     @ApiResponse(
             responseCode = "200",
@@ -34,28 +35,9 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody KakaoAccountIdDto kakaoAccountIdDto) {
-        Member member = memberService.login(kakaoAccountIdDto.getKakaoAccountId());
+        LoginResponseDto loginResponseDto = memberService.login(kakaoAccountIdDto.getKakaoAccountId());
 
-        return ResponseEntity.ok(member.getLevel());
-    }
-
-    @Operation(
-            summary = "회원가입",
-            description = "카카오 계정 아이디로 회원가입을 합니다."
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "회원가입에 성공하였습니다."
-    )
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody KakaoAccountIdDto kakaoAccountIdDto) {
-        try {
-            Member member = memberService.signUp(kakaoAccountIdDto.getKakaoAccountId());
-            return ResponseEntity.ok(member.getLevel());
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok(loginResponseDto);
     }
 
     @Operation(
