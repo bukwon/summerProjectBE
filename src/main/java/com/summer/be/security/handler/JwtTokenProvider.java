e7713cbfd4f626867fb493a56cc5ecfaab23dd1d
t05h19@naver.com <t05h19@naver.com>
2024-08-19 20:47:47 +0900

    fix: 토큰 프로바이더 수정

--- src/main/java/com/summer/be/security/handler/JwtTokenProvider.java
+++ src/main/java/com/summer/be/security/handler/JwtTokenProvider.java
@@ -1,21 +1,31 @@
 package com.summer.be.security.handler;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.summer.be.member.domain.Authority;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 public class JwtTokenProvider {
     private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
-    private final Key key;
+    private static final String AUTHORITIES_KEY = "auth";
+    private static Key key;
 
     public JwtTokenProvider(@Value("${custom.jwt.secretKey}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
@@ -34,6 +44,7 @@ public class JwtTokenProvider {
         try {
             return Jwts.builder()
                     .setSubject(subject) //카카오 uid
+                    .claim(AUTHORITIES_KEY, "ROLE_USER")
                     .setExpiration(expiredAt)
                     .signWith(key, SIGNATURE_ALGORITHM)
                     .compact();
@@ -41,5 +52,56 @@ public class JwtTokenProvider {
             log.info("Token generation failed", e);
             throw new RuntimeException("Token generation failed", e);
         }
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = getClaimsFormToken(accessToken);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    public static boolean isValidToken(String token) {
+        try{
+            Claims claims = getClaimsFormToken(token);
+
+            log.info("expireTime : " + claims.getExpiration());
+            log.info("loginId : " + claims.getSubject());
+
+            return true;
+        }
+        catch (ExpiredJwtException expiredJwtException){
+            log.error("Token Expired", expiredJwtException);
+            return false;
+        }
+        catch (JwtException jwtException){
+            log.error("Token Tampered", jwtException);
+            return false;
+        }
+        catch (NullPointerException npe){
+            log.error("Token is null", npe);
+            return false;
+        }
+    }
+
+    public static Claims getClaimsFormToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public static String getUserIdFromToken(String token) {
+        Claims claims = getClaimsFormToken(token);
+        return claims.getSubject();
     }
 }
