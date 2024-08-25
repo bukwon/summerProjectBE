407f37e576c8ae147bdea41a797d98ec0cc218b9
t05h19@naver.com <t05h19@naver.com>
2024-08-25 23:35:41 +0900

    fix: redis를 사용함에 따라 변경

--- src/main/java/com/summer/be/security/refreshToken/RefreshTokenRepository.java
+++ src/main/java/com/summer/be/security/refreshToken/RefreshTokenRepository.java
@@ -1,9 +1,10 @@
 package com.summer.be.security.refreshToken;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByKey(String key); //KakaoAccountId로 토큰 찾기
 }
+
